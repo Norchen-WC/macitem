fbd8f183312c9b7214a92e2a93ea7e4262793a52
JiangQi <1310123269@qq.com>
2019-09-12 12:04:07 +0800

    xiangmu

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registerRouter = require('./routes/register')
 var macmainRouter = require('./routes/macmain')
+var macairRouter = require('./routes/macair')
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register',registerRouter)
 app.use('/macmain',macmainRouter)
+app.use('/macair',macairRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
